d12c4a504badd7930ab22f38c1b3b0fb8a98d3d7
IvanOmelchenkoIP <io25112002@gamil.com>
2023-03-30 08:24:07 +0300

    Add chain translation
    Add translation from number to linguistic chain

--- lab1/lab1/main.c
+++ lab1/lab1/main.c
@@ -8,14 +8,17 @@
 
 int main()
 {
-    //char alphabet[] = { "A", "B", "C", "D", "E", "F" };
+    char * alphabet[ALPHABET_POWER] = { "A", "B", "C", "D", "E", "F" };
     int numbers[] = {13, 1, 2, 15, 3, 12, 4, 5, 6, 7, 8, 14, 9, 10};
-
+    int interval[ALPHABET_POWER][2];
+    int numbersToIntervals[sizeof(numbers) / sizeof(numbers[0])];
+    char * lingChain[sizeof(numbers) / sizeof(numbers[0])];
     MergeSort(numbers, 0, sizeof(numbers) / sizeof(numbers[0]) - 1);
-
     printf("Hello world!\n");
     for (int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++) {
         printf("%d %f\n", numbers[i], LaplaceFn(numbers[i]));
     }
+
+
     return 0;
 }
---
+++ lab1/lab1/translator.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+void translate(int numIntervals[], char * alphabet[], char * lingChain[]);
+void printLingChain(char * lingChain[]);
